b14fabf5eb109e0b3fdcf1b8eea4269d25e39ce7
jeff <jeff@giftbit.com>
2019-11-07 09:08:32 -0800

    Build project.

---
+++ distjs/functions/FindLastIndex.js
@@ -0,0 +1,28 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const RuleFunction_1 = require("./RuleFunction");
+const LambdaContext_1 = require("./LambdaContext");
+class FindLastIndex extends RuleFunction_1.RuleFunction {
+    invoke(args, context) {
+        if (args.length < 2 || args[1].type !== "Lambda") {
+            return -1;
+        }
+        const list = args[0].getValue(context);
+        if (!Array.isArray(list)) {
+            return -1;
+        }
+        const lambda = args[1];
+        if (lambda.paramNames.length < 1) {
+            return -1;
+        }
+        const lambdaContext = new LambdaContext_1.LambdaContext(context, {});
+        for (let ix = list.length - 1; ix >= 0; ix--) {
+            lambdaContext.setLambdaParamValue(lambda, 0, list[ix]);
+            if (lambda.getValue(lambdaContext)) {
+                return ix;
+            }
+        }
+        return -1;
+    }
+}
+exports.FindLastIndex = FindLastIndex;
